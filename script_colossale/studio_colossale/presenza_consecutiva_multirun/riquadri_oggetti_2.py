2128b2d598dd998b91db081232e6df11afb2dc3d
lorysimeone2000 <62389282+lorysimeone2000@users.noreply.github.com>
2026-04-16 01:16:43 +0200

    68451

--- script_colossale/studio_colossale/presenza_consecutiva_multirun/riquadri_oggetti_2.py
+++ script_colossale/studio_colossale/presenza_consecutiva_multirun/riquadri_oggetti_2.py
@@ -21,7 +21,61 @@ from matplotlib.patches import Circle
 from matplotlib.colors import LogNorm
 import astropy.coordinates as coord
 from datetime import datetime, timedelta
+import pandas as pd
+from photutils.background import Background2D, MedianBackground
+from astropy.convolution import convolve
+from photutils.segmentation import make_2dgaussian_kernel
+import matplotlib
 
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+from photutils.segmentation import SourceCatalog
+from photutils.aperture import aperture_photometry, CircularAperture
+import numpy as np
+import time
+import os
+import sys
+import gc
+from tqdm import tqdm
+from astropy.wcs import WCS
+from astropy.wcs.utils import proj_plane_pixel_scales
+from astropy.io import fits
+from astropy.stats import sigma_clipped_stats
+from photutils.segmentation import SourceFinder
+import warnings
+from astropy.wcs import FITSFixedWarning
+from photutils.datasets import make_100gaussians_image
+from scipy.optimize import curve_fit
+from photutils.segmentation import detect_sources
+from astropy.visualization import SqrtStretch
+from astropy.visualization.mpl_normalize import ImageNormalize
+from photutils.segmentation import deblend_sources
+from astropy.visualization import simple_norm
+from astropy.convolution import Gaussian2DKernel
+from astropy.utils.data import download_file
+from astropy.table import Table, vstack
+from photutils.detection import find_peaks
+from astropy.coordinates import SkyCoord
+import astropy.coordinates as coord
+from astropy.coordinates import search_around_sky
+import astropy.units as u
+from astropy.utils.data import get_pkg_data_filename
+from astropy.wcs.wcsapi import SlicedLowLevelWCS
+from astroquery.vizier import Vizier
+from astropy.coordinates import Angle
+from shapely.geometry import Point, Polygon
+from astropy.io.fits.verify import VerifyWarning
+from astropy.utils.exceptions import AstropyUserWarning
+from scipy.ndimage import label
+from pathlib import Path
+
+# gestisco i warning ignorandoli
+warnings.filterwarnings('ignore', category=FITSFixedWarning)
+warnings.filterwarnings('ignore', message='.*failed to converge.*', category=UserWarning)
+warnings.simplefilter('ignore', category=FITSFixedWarning)
+warnings.filterwarnings('ignore', category=VerifyWarning)
+warnings.filterwarnings('ignore', category=pd.errors.PerformanceWarning)
 warnings.filterwarnings('ignore', category=RuntimeWarning)
 warnings.filterwarnings('ignore', category=FITSFixedWarning)
 warnings.filterwarnings('ignore', message='.*failed to converge.*', category=UserWarning)
